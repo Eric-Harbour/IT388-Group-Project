156b37e0c22a737c93ec6f9286e1c3d5f01c5cd3
Nate <nathanghilbert@gmail.com>
2026-04-08 18:34:57 -0500

    initial mpi commit

--- src/mpi_main.cpp
+++ src/mpi_main.cpp
@@ -10,9 +10,30 @@ int main(int argc, char** argv) {
 
     int world_rank;
     int world_size;
+    int imgWidth, imgHeight, pixelComponent;
+    unsigned char *extractedPixels = nullptr;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Manager validates input arguments and loads image
+    if (world_rank == 0) {    
+        if (argc != 3)  {
+            std::cout << "Usage: ./" << argv[0] << " <number-of-processors> <image-file-name>" << std::endl;
+        }
+        char *image_filename = argv[2];
+
+        // Extract pixels with pixelComponent=4 (red, green, blue, alpha)
+        extractedPixels = stbi_load(image_filename, &imgWidth, &imgHeight, &pixelComponent, 4);
+        if (!extractedPixels) {
+            std::cerr << "Failed to load image" << std::endl;
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+    }
+
+    // Broadcast image width and height from the manager to all other processes
+    MPI_Bcast(&imgWidth,  1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&imgHeight, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     std::cout << "Hello from rank " << world_rank << " of " << world_size << std::endl;
 
