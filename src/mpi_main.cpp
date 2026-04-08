32f8d537c1c34a9c791ea78e5c66a13b0f621b99
Garrett Blankenship <g.blankenship456@gmail.com>
2026-04-08 18:03:12 -0500

    Add image.hpp/image.cpp

--- src/mpi_main.cpp
+++ src/mpi_main.cpp
@@ -1,5 +1,9 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <iostream>
 #include <mpi.h>
+#include "stb_image.h"
+#include "stb_image_write.h"
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
--- src/omp_main.cpp
+++ src/omp_main.cpp
@@ -3,11 +3,14 @@
  * 1. How many threads you want to run (argv[1]: nThreads)
  * 2. What image you want to process (argv[2]: imgName)
  */
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 
 #include <iostream>
 #include <omp.h>
-#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#include "stb_image_write.h"
+
 // #include <string.h>
 
 int main(int argc, char **argv)
