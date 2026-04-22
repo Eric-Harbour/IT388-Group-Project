8121a3eea12fdc9f8607cb1b2906801bbd0c8e66
Garrett Blankenship <g.blankenship456@gmail.com>
2026-04-22 10:26:38 -0500

    Fixed script

--- scripts/load_modules.sh
+++ scripts/load_modules.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-module load gcc/11.4.0
+echo "Loading modules..."
+echo "GCC"
+module load gcc/10.2.0
+
+echo "CMake"
+module load cmake/3.21.4
+
+echo "OpenMPI"
 module load openmpi/4.1.3
-module load gpu/0.17.3b
+
+echo "GPU"
+module load gpu/0.15.4
+
+echo "CUDA"
 module load cuda11.7/toolkit/11.7.1
